a75d0ea699c6185966cb7835080703456edb7456
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-19 16:14:35 -0400

    Added an index page

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pets 2</title>
+</head>
+<body>
+
+<h3>My pets</h3>
+
+<a href ='order'> Order a pet </a>
+
+</body>
+</html>
+
+<?php
+
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file autoload.php
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class/ instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-free error reporting/Debugging
+$f3->set('DEBUG',3);
+
+//Define a default route (use backlash / )
+$f3->route('GET /', function()
+{
+    //Display a view-set view as new template and echo out the view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat free F3
+$f3->run();
