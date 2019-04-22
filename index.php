e25eb1e06f9984a0cdcb72dbc6b0d1cac3de9b2b
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-22 03:29:21 -0400

    Added a new route

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -53,8 +54,25 @@ $f3->route('GET /@animal', function($f3, $params) {
 });
 
 $f3->route('GET /order', function() {
+
     $view = new Template();
     echo $view->render('views/form1.html');
+});
+
+$f3->route('POST /color', function() {
+
+    $_SESSION['animal'] = $_POST['animal'];
+
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+$f3->route('POST /summary', function() {
+
+    $_SESSION['color'] = $_POST['color'];
+
+    $view = new Template();
+    echo $view->render('views/results.html');
 });
 
 //Run fat free F3
